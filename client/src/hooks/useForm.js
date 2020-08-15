945566506c9161399b719635e76e8dc9152de036
Dodger-42 <kevin.bing.barber@gmail.com>
2020-08-14 17:42:56 -0700

    third

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,22 @@
 // write your custom hook here to control your checkout form
+
+
+import {useEffect} from "react";
+
+
+const useForm = (key) => {
+
+        const [Form, setForm] = useCheckoutForm(key, false);
+        
+        useEffect(()=>{
+            if (plant){
+                document.querySelector("item").classList.add("plant");
+            } else {
+                document.querySelector("item").classList.remove("plant");
+            }
+        }, [Form]);
+
+        return [Form, setForm];
+}
+
+export default useForm;
