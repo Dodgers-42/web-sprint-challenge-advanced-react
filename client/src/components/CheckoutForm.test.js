19143cbe71514a572c6b12184abda26c881d1fe9
Dodger-42 <kevin.bing.barber@gmail.com>
2020-08-17 16:37:36 -0700

    sixth

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,65 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+});
 
-test("form shows success message on submit with form details", () => {});
+
+test("form shows success message on submit with form details", () => {
+// // Arrange
+// const { getByTestId } = render(<CheckoutForm />);
+  
+// // Act
+// const firstName = getByTestId(/first/i);
+// console.log(firstName.textContent);
+
+// Assert
+// expect(firstName.textContent).toEqual("Correct Amount letters");
+
+
+// // Arrange
+// const { getByTestId } = render(<CheckoutForm />);
+  
+// // Act
+// const lastName = getByTestId(/last/i);
+// console.log(lastName.textContent);
+
+
+// // Arrange
+// const { getByTestId } = render(<CheckoutForm />);
+  
+// // Act
+// const address = getByTestId(/address/i);
+// console.log(address.textContent);
+
+
+// // Arrange
+// const { getByTestId } = render(<CheckoutForm />);
+  
+// // Act
+// const city = getByTestId(/city/i);
+// console.log(city.textContent);
+
+
+
+// // Arrange
+// const { getByTestId } = render(<CheckoutForm />);
+  
+// // Act
+// const state = getByTestId(/state/i);
+// console.log(state.textContent);
+
+
+
+// // Arrange
+// const { getByTestId } = render(<CheckoutForm />);
+  
+// // Act
+// const zipCode = getByTestId(/zipCode/i);
+// console.log(zipCode.textContent);
+
+
+
+});
+
