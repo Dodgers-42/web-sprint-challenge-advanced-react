0269ded5238eb4226a9c7e202cd607ff467a56be
Dodger-42 <kevin.bing.barber@gmail.com>
2020-08-17 18:35:24 -0700

    testing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import { act } from "react-dom/test-utils";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -20,37 +21,21 @@ console.log(firstName.textContent);
 // Assert
 // expect(firstName.textContent).toEqual("Correct Amount letters");
 
-
-
-  
 // // Act
 const lastName = getByTestId(/last/i);
 console.log(lastName.textContent);
 
-
-
-  
 // // Act
 const address = getByTestId(/address/i);
 console.log(address.textContent);
-
-
-
   
 // // Act
 const city = getByTestId(/city/i);
 console.log(city.textContent);
-
-
-
-
   
 // // Act
 const state = getByTestId(/state/i);
 console.log(state.textContent);
-
-
-
 
 // // Act
 const zipCode = getByTestId(/zipCode/i);
@@ -60,3 +45,20 @@ console.log(zipCode.textContent);
 
 });
 
+describe("submit", () => {
+    describe("shows sucessful message", () => {
+        it('calls the onSummit function', async () => {
+            const mockOnSubmit = jest.fn()
+            const {getByText, getByRole} = render(<CheckoutForm onSubmit={mockOnSubmit}/>)
+
+            await act(async () => {
+                fireEvent.change(getByText("message"), {target: {value: "message" }})
+            })
+            await act(async () => {
+                fireEvent.click(getByRole("button"))
+            })
+
+            expect(mockOnSubmit).toHaveBeenCalled()
+        })
+    })
+})
